763dc657fbfe80355287be1fbb11c39fb17f4471
Francis <francisgomas721@gmail.com>
2022-09-23 00:19:06 +1200

    Initial Commit

---
+++ wwwroot/js/main.js
@@ -0,0 +1,15 @@
+﻿jQuery(document).ready(function () {
+	$('.dobpicker').datepicker({
+		autoclose: true,
+	});
+
+	$("#jobs-application-resume").fileinput({
+		required: true,
+		browseClass: "btn btn-secondary",
+		browseIcon: "",
+		removeClass: "btn btn-danger",
+		removeLabel: "",
+		removeIcon: "<i class='icon-trash-alt1'></i>",
+		showUpload: false
+	});
+})
